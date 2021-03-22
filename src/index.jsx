d836522ecf18ef073d4357e07772061c31152329
thiagoos16 <tos@icomp.ufam.edu.br>
2021-03-21 21:06:22 -0400

    add react initial structure

--- src/index.jsx
+++ src/index.jsx
@@ -1,2 +1,5 @@
-import React from 'react';
+// import React from 'react'; não precisa, pois está configurado no babel automatic
+import {render} from 'react-dom';
 import { App } from './App';
+
+render(<App />, document.getElementById('root'));
