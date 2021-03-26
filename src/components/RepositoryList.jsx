e402f6311e5e63811d68e87e720bedef590bb209
thiagoos16 <tos@icomp.ufam.edu.br>
2021-03-25 20:22:46 -0400

    add repository list component

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,28 @@
+const repositoryName = 'unform';
+
+export function RepositoryList() {
+    return (
+        <section>
+            <h1>Lista de respositórios</h1>
+
+            <ul>
+                <li>
+                    <strong>{repositoryName}</strong>
+                    <p>Forms in react</p>
+
+                    <a haref="">
+                        Acessar repositório
+                    </a>
+                </li>
+                <li>
+                    <strong>unform</strong>
+                    <p>Forms in react</p>
+
+                    <a haref="">
+                        Acessar repositório
+                    </a>
+                </li>
+            </ul>
+        </section>
+    )
+}
